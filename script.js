f5da853ea9e2d610ac02728831e62f1f6dbaa38a
RayanHakeem <49831806+RayanHakeem@users.noreply.github.com>
2019-04-25 00:06:58 +0300

    HW4

--- script.js
+++ script.js
@@ -26,21 +26,54 @@ randomQuote();
 
 
 // Part 1
-
+var shortTitle = document.querySelector('#main-title');
+ shortTitle.textContent = 'Welcome all, I am DOM';
 
 // Part 2
-
+var newBody = document.querySelector('body');
+newBody.style.backgroundColor = 'dark gray';
 
 // Part 3
-
+var newFSize=document.querySelectorAll('.special-title');
+for(i = 0 ; i < newFSize.length ; i++){
+  newFSize[i].style.fontSize ='2rem'
+}
 
 // Part 4
+var SL = document.querySelectorAll('.side-bar');
+var Ul = document.querySelectorAll('ul');
+var NL = document.createElement('li');
+var AC = document.createTextNode('jeddah');
+NL.appendChild(AC);
+UL.appendChild(NL);
+SL.appendChild(UL);
+document.body.appendChild(SL);
 
 
 // Part 5
-
+var newDiv = document.createElement('div')
+newDiv.classList.add('blog-post')
+newDiv.classList.add('red');
+var h2 = document.createElement('h2');
+var p = document.createElement('p');
+var city1 = document.createTextNode('Drive ir die');
+document.querySelector('.main').appendChild(newDiv);
+p.appendChild(city1);
+h2.appendChild(p);
+newDiv.appendChild(h2);
+newDiv.appendChild(h2);
 
 // Part 6
-
+var FTList = document.querySelector('#favorite-things');
+for (var i = 0; i < favoriteThings.length; i++) {
+    var FT = favoriteThings[i];
+    var Listli = document.createElement('li');
+    var Textli = document.createTextNode(FT);
+    Listli.appendChild(Textli);
+    FTList.appendChild(Listli);
 
 // Part 7
+function randomQuote() 
+var randomIndex = Math.floor(Math.random()* quotes.length)
+document.querySelector('#quote-of-the-day').textContent= '"' +quotes[randomIndex] + '"';
+};
