9f037f2a52d126b543cca8e1443167a1fd02169d
Hoda Altalohi <hooda12347@hotmail.com>
2019-04-24 20:09:58 +0300

    finished homework

--- script.js
+++ script.js
@@ -26,21 +26,71 @@ randomQuote();
 
 
 // Part 1
-
-
+  //  document.body.appendChild('Welcome to my homepage');
+document.title = "psge"
 // Part 2
-
-
+document.body.style.background = 'cgray'
 // Part 3
-
-
+var font = document.querySelectorAll('.special-title');
+for ( var i = 0 ; i <font.length ; i ++ ){
+  font[i].style.fontSize = '2rem' ;
+}
 // Part 4
 
 
+var divClass = document.querySelectorAll('.side-bar')[1];
+var ulList = document.querySelectorAll('ul')[1];
+var newFavList = document.createElement('li');
+var newCity = document.createTextNode('Riyadh');
+
+ulList.appendChild(newFavList);
+newFavList.appendChild(newCity);
+divClass.appendChild(ulList);
+this.document.body.appendChild(ulList);
+
 // Part 5
+var newDiv = document.createElement('div');
+newDiv.classList.add('blog-post');
+newDiv.classList.add('purple');
+document.querySelector('.main').appendChild(newDiv);
+
+var newH2 = document.createElement('h2');
+newDiv.appendChild(newH2);
+var textH2 = document.createTextNode('Riyadh');
+newH2.appendChild(textH2);
+
+var newp = document.createElement('p');
+newDiv.appendChild(newp);
+var textP = document.createTextNode('Capital of Saudi Arabia');
+newp.appendChild(textP); 
 
 
 // Part 6
-
+/*
+var favoriteThings = [
+ 'Fast cars',
+ 'Muscles',
+ 'Sleeveless t-shirts',
+ 'Revving my engine',
+ 'Living my life a quarter mile at a time',
+ 'Staring people down'
+]
+*/
+var takeId = document.querySelector('#favorite-things');
+for(var i=0; i<favoriteThings.length; i++){
+ var takeFavourit = favoriteThings[i];
+ var newLI = document.createElement('li');
+ var textNewLI = document.createTextNode(takeFavourit);
+ newLI.appendChild(textNewLI);
+ takeId.appendChild(newLI);
+}
 
 // Part 7
+var methodQuote = document.querySelector('#quote-of-the-day');
+methodQuote.addEventListener('click', function(event){
+event.preventDefault();
+
+console.log(randomQuote());
+console.log('click');
+
+});
