fc738c3aa7dd9fbbc1730506a71a92ed8f2f7b3b
Mohammed <mo842002sa@yahoo.com>
2019-04-24 22:30:09 +0300

    Mohammed

--- script.js
+++ script.js
@@ -27,20 +27,41 @@ randomQuote();
 
 // Part 1
 
-
+ document.body.appendChild('Welcome to my homepage');
+document.title = "psge"
 // Part 2
+function changeBackground(color) {
+  document.body.style.background = color;
+}
 
+window.addEventListener("load",function() { changeBackground('#a1edff'); });
 
 // Part 3
+function js_style() {
 
+  document.getElementById("special-title").style.fontSize = "2rem";
+}
 
-// Part 4
+//  Part 4
 
+  
 
 // Part 5
-
+var ndiv = document.createElement('div');
+var newH2 = document.createElement('h2');
+var text2 = document.createTextNode('New York');
+	ndiv.classList.add('blog-post');
+	ndiv.classList.add('purple');
+	document.querySelector('.main').appendChild(ndiv);
+	ndiv.appendChild(div2);
+	div2.appendChild(text2);
+	
 
 // Part 6
 
 
 // Part 7
+function randomQuote() {
+  var randomIndex = Math.floor(Math.random() * quotes.length)
+  document.querySelector('#quote-of-the-day').textContent = '"' + quotes[randomIndex] + '"';
+};
