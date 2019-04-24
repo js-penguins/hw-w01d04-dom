8c58eae17f6556641338d47791284cf36e9651f9
haithemalbetairi <haithem.t.albetairi@gmail.com>
2019-04-24 19:04:24 +0300

    DONE :D

--- script.js
+++ script.js
@@ -25,22 +25,59 @@ function randomQuote() {
 randomQuote();
 
 
-// Part 1
+// helper function to quickly create elements with text and css class
+// can pass it a single style or an array with css styles
+// (String:String:List of Strings) -> HTML element
+function elementFactory(tag, text, css) {
+  var element = document.createElement(tag);
+  var innerHTML = document.createTextNode(text);
 
+  element.appendChild(innerHTML);
+  if(css !== '') {
+    if(typeof css === 'string') {
+      element.classList.add(css);
+    } else {
+      css.forEach(function(style) {
+        element.classList.add(style);
+      })
+    }
+  }
+ 
+  return element;
+}
+
+console.log(elementFactory('span', 'it works!', ['lol', 'this-is-awesome']));
+
+// Part 1
+document.querySelector('#main-title').innerHTML = "Dom Toretto's Homepage";
 
 // Part 2
-
+document.body.style.backgroundColor = 'lightgreen';
 
 // Part 3
-
+var specialTitles = document.querySelectorAll('.special-title');
+specialTitles.forEach(function (title) {
+  title.style.fontSize = '2rem';
+})
 
 // Part 4
-
+document.querySelector('#past-races').appendChild(elementFactory('li', 'Riyadh', ''));
 
 // Part 5
+var riyadhDiv = elementFactory('div', '', ['blog-post', 'purple']);
+var riyadhH2 = elementFactory('h1', 'Riyadh','');
+var riyadhP = elementFactory('p', "RACED IN A SANDSTORM WHILE LISTENING TO DARUDE'S SANDSTORM", '');
+riyadhDiv.appendChild(riyadhH2);
+riyadhDiv.appendChild(riyadhP);
 
+document.querySelector('.main').appendChild(riyadhDiv);
 
 // Part 6
-
+favoriteThings.forEach(function(thing) {
+  document.querySelector('#favorite-things').appendChild(elementFactory('li',thing,''));
+})
 
 // Part 7
+document.querySelector('#quote-title').addEventListener('click', function(event) {
+  randomQuote();
+})
