d6ec473dcb54c0fe01ec89e9b7277a76b2738024
Jackie Casper <casper.jaclyn@gmail.com>
2019-04-24 16:29:55 +0300

    add hw

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
 var quotes = [
   'I live my life a quarter mile at a time',
   'I said a ten-second car, not a ten-minute car',
-  'You can have any brew you want... as long as it\'s a Corona.',
   'You almost had me? You never had me - you never had your car!',
   'I don\'t have friends. I have family.',
   'It don\'t matter if you win by an inch or a mile. Winning\'s winning.'
 ];
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // Random quote of the day generator
-  var randomQuote = function() {
-    document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
-  };
-  randomQuote();
-  
-  // Do all of your work inside the document.addEventListener  
-
-  // Part 1
+var favoriteThings = [
+  'Fast cars',
+  'Muscles',
+  'Sleeveless t-shirts',
+  'Revving my engine',
+  'Living my life a quarter mile at a time',
+  'Staring people down'
+]
 
 
-  // Part 2
+// Random quote of the day generator
+function randomQuote() {
+  var randomIndex = Math.floor(Math.random() * quotes.length)
+  document.querySelector('#quote-of-the-day').textContent = '"' + quotes[randomIndex] + '"';
+};
+
+randomQuote();
 
 
-  // Part 3
+// Part 1
 
 
-  // Part 4
+// Part 2
 
 
-  // Part 5
+// Part 3
 
 
-  // Part 6
+// Part 4
 
 
-  // Part 7
+// Part 5
 
 
-  // Part 8
+// Part 6
 
 
-  // Part 9
-
-
-
-
-});
+// Part 7
