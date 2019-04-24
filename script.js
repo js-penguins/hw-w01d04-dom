72444e9bc15d5ec74fdc4cbaa535af22bf2a298c
waad ali <wabaabdullah@gmail.com>
2019-04-24 23:47:06 +0300

    finish Dom HW

--- script.js
+++ script.js
@@ -26,21 +26,76 @@ randomQuote();
 
 
 // Part 1
-
+function changeText() {
+  var titleText = document.querySelector('#main-title');
+  titleText.innerHTML = 'DOM Toretto';
+  console.log(titleText);
+}
+changeText();
 
 // Part 2
-
+function changColor() {
+  var bodyElement = document.querySelector(".main");
+  bodyElement.style.backgroundColor = 'pink';
+}
+changColor();
 
 // Part 3
-
-
+function changFont() {
+  var titleElement = document.querySelectorAll('.special-title');
+  for (var i = 0; i < titleElement.length; i++) {
+    titleElement[i].style.fontSize = '1.5rem';//2rem was looking unsuitable :) 
+  }
+}
+changFont();
 // Part 4
-
-
+function addCityList(){
+  var citylist=document.getElementsByTagName('ul');
+  //onsole.log(citylist);
+  for (var i=0;i<citylist.length;i++){
+    if(citylist[i].id !== 'favorite-things'){
+      var cityaddnew = document.createElement("li");
+     cityaddnew.appendChild(document.createTextNode("Riyadh"));
+     citylist[i].appendChild(cityaddnew);
+    }
+  }
+}
+addCityList();
 // Part 5
-
-
+function addPost(){
+  var titlePost = document.createElement("h1");
+  titlePost.appendChild(document.createTextNode("Riyadh"));
+  var contentPost = document.createElement("p");
+  contentPost.appendChild(document.createTextNode("I DROVE MY CAR AROUND A PARKING GARAGE"));
+  var newDiv = document.createElement("div"); 
+  newDiv.className="blog-post purple"; 
+  console.log(contentPost);
+  console.log(titlePost);
+  newDiv.appendChild(titlePost);
+  newDiv.appendChild(contentPost);
+  var attachPost= document.querySelector(".main");
+  attachPost.appendChild(newDiv);
+}
+addPost();
 // Part 6
-
-
-// Part 7
+function addList(){
+  var favThing=['Diving','Swimming','Learning','Skiting'];
+  var listItem,postItem;
+  for (var i=0; i<favThing.length;i++){
+    listItem=document.createElement('li');
+    listItem.appendChild(document.createTextNode(favThing[i]));
+    postItem=document.getElementById('favorite-things');
+    postItem.appendChild(listItem);
+  }
+}
+addList();
+// Part7
+function changeQuet(){
+  var ele=document.querySelector('#quote-title');
+  console.log(ele);
+  ele.addEventListener('click',function(event){
+    event.preventDefault();
+    randomQuote();});
+  }
+  
+  changeQuet();
