a8f0cb31dddf18c8814c3522b3ec07879cdbc791
Emtenan Alnasser <emtenan95.1@hotmail.com>
2019-04-25 09:41:02 +0300

    HW4

--- script.js
+++ script.js
@@ -25,22 +25,77 @@ function randomQuote() {
 randomQuote();
 
 
-// Part 1
+// // Part 1
+ 
+  
+  document.title = "Hi";
 
 
-// Part 2
+// // Part 2
+document.body.style.background= "skyblue";
 
 
-// Part 3
+// // Part 3
+function cangeSize(){
+  var change = document.getElementsByClassName("special-title")
+  change.forEche(function(Change1){
+    document.Change1.style.fontSize= "2em"
+      
+  }
+  
+  
+)
 
 
-// Part 4
+// // Part 4
+ 
+   var addlis= document.querySelectorAll("side-bar")[1]
+   var Uli= document.querySelectorAll("ul")[1]
+   var li1= document.createElement("li")
+   var licity= document.createTextNode("Jeddah")
 
 
-// Part 5
+   li1.appendChild("licity")
+   Uli.appendChild("li1")
+   addlis.appendChild("Uli")
 
 
-// Part 6
 
 
-// Part 7
+
+// // Part 5
+var Ndiv = document.createElement('div');
+Ndiv.classList.add('blog-post');
+Ndiv.classList.add('purple');
+var Header1 = document.createElement('h1');
+var p1 = document.createElement('p');
+var pText = document.createTextNode(':$');
+
+p1.appendChild(pText);
+Header1.appendChild(licity);
+Ndiv.appendChild(Header1);
+Ndiv.appendChild(p1);
+
+document.querySelector('.main').appendChild(Ndiv);
+
+
+// // Part 6
+var listt = document.querySelector('#favorite-things');  
+   
+  
+   console.log(listt);
+   for (i=0;i<favoriteThings.length;i++)
+   {
+     var te = favoriteThings[i];
+   var li1 = document.createElement('li');  
+   var li1text = document.createTextNode(te); 
+   li1.appendChild(li1text); 
+    
+   listt.appendChild(li1)
+
+// // Part 7
+
+var Bsubmit =document.querySelector('.special-title');  
+Bsubmit.addEventListener('click', function (event) {  
+      randomQuote()  ;
+    event.preventDefault()})
