85e810b1b69c2b92959e7cf23665b5fa1c02729b
Mohammed <mo842002sa@yahoo.com>
2019-04-28 22:48:11 +0300

    Mohammed

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function randomQuote() {
 };
 
 randomQuote();
+console.log(randomQuote);
 
 
 // Part 1
@@ -37,31 +38,53 @@ function changeBackground(color) {
 window.addEventListener("load",function() { changeBackground('#a1edff'); });
 
 // Part 3
-function js_style() {
+function jStyle() {
 
   document.getElementById("special-title").style.fontSize = "2rem";
 }
 
 //  Part 4
 
-  
+var dClass = document.querySelectorAll('.side-bar')[1];
+var ulList = document.querySelectorAll('ul')[1];
+var nFList = document.createElement('li');
+var nCity = document.createTextNode('New York');
+nFList.appendChild(nCity);
+ulList.appendChild(nFavList);
+dClass.appendChild(ulList);
+this.document.body.appendChild(ulList);
+var selList = document.querySelector('#city');
+	addToList(newList,newCity,selList);
+console.log(nCity);
 
 // Part 5
 var ndiv = document.createElement('div');
 var newH2 = document.createElement('h2');
-var text2 = document.createTextNode('New York');
-	ndiv.classList.add('blog-post');
-	ndiv.classList.add('purple');
+var Ntext = document.createTextNode('New York');
+var listt = document.querySelector('#favorite-things'); 
+	ndiv.cList.add('blog-post');
+	ndiv.cList.add('purple');
 	document.querySelector('.main').appendChild(ndiv);
 	ndiv.appendChild(div2);
-	div2.appendChild(text2);
-	
+  div2.appendChild(Ntext);
+  
+
+
 
 // Part 6
+var fList = document.querySelector('#favorite-things')
+favorite-hings.forEach(function(i){
+  var nItem = document.createElement('li');
+  var txts = document.createTextNode(i);
+  fList.appendChild(nItem)
+  nItem.appendChild(txts)
+	console.log(fList);
 
 
 // Part 7
 function randomQuote() {
   var randomIndex = Math.floor(Math.random() * quotes.length)
   document.querySelector('#quote-of-the-day').textContent = '"' + quotes[randomIndex] + '"';
+  console.log(event);
 };
+})
